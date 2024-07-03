8e82d47f24906827ba47ed0aec2676dfaf25d633
PetarV1 <petar.vasilj99@gmail.com>
2024-07-03 14:12:04 +0200

    Created sections and added assets

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,15 @@
-export default function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
-}
+const App = () => (
+  <main className="relative">
+    Nav
+    <section className="wide:padding-r padding-b xl:padding-1">Hero</section>
+    <section className="padding"></section>
+    <section className="padding">SuperQuality</section>
+    <section className="padding-x py-10">Services</section>
+    <section className="padding">SpecialOffers</section>
+    <section className="padding bg-pale-blue">CustomerReviews</section>
+    <section className="padding-x sm:py-32 py-1 w-full">Subscribe</section>
+    <section className="padding">Footer</section>
+  </main>
+);
+
+export default App;
