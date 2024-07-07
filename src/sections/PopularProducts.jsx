c309714b7069165752d6e4149245cc2a181e3539
PetarV1 <petar.vasilj99@gmail.com>
2024-07-07 12:38:54 +0200

    Added PopularProducts section

--- src/sections/PopularProducts.jsx
+++ src/sections/PopularProducts.jsx
@@ -1,5 +1,28 @@
+import { products } from "../constants";
+import PopularProductCard from "../components/PopularProductCard";
+
 const PopularProducts = () => {
-  return <div>PopularProducts</div>;
+  return (
+    <section id="products" className="max-container max-sm:mt-12">
+      <div className="flex flex-col justify-start gap-5">
+        <h2 className="text-4xl font-palanquin font-bold">
+          Our <span className="text-coral-red">Popular</span> Products
+        </h2>
+        <p className="lg:max-w-lg mt-2 font-montserrat text-slate-gray">
+          Discover our exclusive range of footwear designed for comfort, style,
+          and durability. Each pair is crafted with precision using the finest
+          materials to ensure exceptional quality. Whether you're looking for
+          casual sneakers, elegant formal shoes, or sturdy outdoor boots, our
+          collection offers something for every occasion.
+        </p>
+      </div>
+      <div className="mt-16 grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 sm:gap-4 gap-14">
+        {products.map((product) => (
+          <PopularProductCard key={product.name} {...product} />
+        ))}
+      </div>
+    </section>
+  );
 };
 
 export default PopularProducts;
